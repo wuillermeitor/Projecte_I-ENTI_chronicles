43452d795fbd43a7b1debe5fff4b6df7a5ed321c
Guillermo Sánchez <guillermosanchezsanchez@enti.cat>
2017-06-06 17:55:24 +0200

    añadido esbirro megaman y otras movidas

---
+++ ENTI chronicles - Proyecto I/Assets/Scripts/Enemies/BichoMegaman.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BichoMegaman : MonoBehaviour {
+    public float enemyspeed;
+    private bool IsGoingDown;
+
+    public GameObject StartPoint;
+    public GameObject EndPoint;
+    private Player player;
+    private BulletGen bullet;
+    private FBGen fireball;
+    
+    //Encontrar contacto con el jugador
+    public Transform HerocontactCkeck;
+    public float HerocontactcheckRadius;
+    public LayerMask whatisHerocontact;
+    private bool Herocontact;
+    
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        bullet = FindObjectOfType<BulletGen>();
+        fireball = FindObjectOfType<FBGen>();
+
+        if (IsGoingDown)
+        {
+            transform.position = StartPoint.transform.position;
+        }
+        else
+        {
+            transform.position = EndPoint.transform.position;
+        }
+    }
+
+
+    void Update()
+    {
+        Herocontact = Physics2D.OverlapCircle(HerocontactCkeck.position, HerocontactcheckRadius, whatisHerocontact);
+        
+        movimiento();
+        contacto();
+    }
+
+    void movimiento()
+    {
+        if (IsGoingDown)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, EndPoint.transform.position, enemyspeed * Time.deltaTime);
+
+            if (transform.position == EndPoint.transform.position)
+            {
+                IsGoingDown = false;
+            }
+        }
+
+        if (!IsGoingDown)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, StartPoint.transform.position, enemyspeed * Time.deltaTime);
+            if (transform.position == StartPoint.transform.position)
+            {
+                IsGoingDown = true;
+            }
+        }
+    }
+
+    void contacto()
+    {
+        if (player.inmunity == false)
+        {
+            //CONTACTO
+            if (Herocontact == true)
+            {
+                player.touched = true;
+            }
+        }
+    }
+}
